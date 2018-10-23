f176831cdfdb4f6219b9b9150f66ff09cd0bc961
oliviaosimon <42871888+oliviaosimon@users.noreply.github.com>
2018-10-23 11:36:02 -0400

    Updated from Brython Server: 10/23/2018 11:36:01 AM

--- conway.py
+++ conway.py
@@ -1,7 +1,7 @@
 """
 conway.py
 Author: Olivia Simon
-Credit: <list sources used, if any>
+Credit: 
 Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
